ab7954844b5209e20f18e932f9ebb1c18ec514b7
Justin Gilmer <justin.b.gilmer@vanderbilt.edu>
2021-01-25 14:20:30 -0600

    Add support to convert to openff FrozenMolecule
    Previously, there was no support to convert a non-parametrized gmso
    Topology to an openforcefield FrozenMolecule.
    
    This pull request supports converting a non-parametrized GMSO topology
    to an openforcefield FrozenMolecule.
    
    Note, tests are currently missing, but conversion is successful.

--- interop/interop.py
+++ interop/interop.py
@@ -1,5 +1,8 @@
 from mbuild.lib.molecules import Ethane
+from openforcefield.topology import FrozenMolecule
+from collections import OrderedDict
 from gmso import Topology as GMSOTopology
+from gmso import Atom
 from gmso.external import from_mbuild
 from openff.toolkit.topology import Topology as OpenFFTopology
 from openff.toolkit.topology import Molecule
@@ -39,9 +42,86 @@ class InterPackageConverter:
         )
 
         if gmso_toplogy.box is not None:
-            openff_topology.box_vectors = gmso_toplogy.box.get_vectors().in_units('angstrom').value * simtk_unit.angstroms
+            openff_topology.box_vectors = gmso_toplogy.box.get_vectors().in_units(
+                'angstrom').value * simtk_unit.angstroms
 
         return openff_topology
+
+    @staticmethod
+    def to_openff_frozenmolecule(topology=None):
+        """
+        Return an openforcefield toolkit FrozenMolecule object.
+
+        This will return an OpenForceField Toolkit FrozenMolecule from a GMSO
+        topology.
+
+        Parameters
+        ----------
+        topology : `gmso.Topology` object
+            An untyped Topology object
+
+        Returns
+        -------
+        molecule : An `openff.toolkit.topology.FrozenMolecule` object
+            A FrozenMolecule from the openforcefield toolkit
+
+        NOTE:
+        -----
+        The current implementation converts a `Topology` to a `FrozenMolecule`
+        using the `from_dict` method.
+        """
+
+        if not topology:
+            raise TypeError(f'Expected: {type(GMSOTopology)}, '
+                            f'Provided: {type(topology)}')
+        mol_dict = OrderedDict()
+        mol_dict['name'] = topology.name
+        mol_dict['atoms'] = _create_atom_container(topology)
+        mol_dict['bonds'] = _create_bond_container(topology)
+        mol_dict['virtual_sites'] = tuple()
+        mol_dict['partial_charges'] = None
+        mol_dict['conformers'] = None
+        mol_dict['properties'] = None
+
+        return FrozenMolecule.from_dict(mol_dict)
+
+
+def _create_atom_container(topology):
+    if not isinstance(topology, GMSOTopology):
+        raise TypeError(f'Expected {type(GMSOTopology)}, '
+                        f'Provided: {type(topology)}')
+    atom_tuple = tuple()
+    for atom in topology.sites:
+        if not isinstance(atom, Atom):
+            raise TypeError(f'Expected {type(Atom)}, '
+                            f'Provided: {type(atom)}')
+        # NOTE: This is not the correct method to calculate "formal charge"
+        charge = 0.0 if (atom.charge is None) else atom.charge.value
+        charge *= simtk_unit.elementary_charge
+        tmp_dict = {'atomic_number': atom.element.atomic_number,
+                    'formal_charge': charge,
+                    'is_aromatic': False}
+        atom_tuple += (tmp_dict,)
+
+    return atom_tuple
+
+
+def _create_bond_container(topology):
+    if not isinstance(topology, GMSOTopology):
+        raise TypeError(f'Expected {type(GMSOTopology)}, '
+                        f'Provided: {type(topology)}')
+
+    bond_tuple = tuple()
+    for bond in topology.bonds:
+        tmp_dict = dict()
+        tmp_dict['atom1'] = topology.get_index(bond.connection_members[0])
+        tmp_dict['atom2'] = topology.get_index(bond.connection_members[1])
+        tmp_dict['bond_order'] = 1
+        tmp_dict['is_aromatic'] = False
+
+        bond_tuple += (tmp_dict,)
+
+    return bond_tuple
 
 
 def test_conversion():
@@ -49,6 +129,12 @@ def test_conversion():
     ethane_gmso.name = 'ethane'
     InterPackageConverter.convert_to_openff(ethane_gmso)
 
+def test_openff_conversion():
+    ethane_gmso = from_mbuild(Ethane())
+    ethane_gmso.name = 'ethane'
+    InterPackageConverter.to_openff_frozenmolecule(ethane_gmso)
+
 
 if __name__ == '__main__':
     test_conversion()
+    test_openff_conversion()
